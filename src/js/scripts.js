e61c381d3fa7e1c7723fd923d46430f60ea6f8e1
Michael Cheung <michaelcheung@nhs.net>
2022-10-13 20:42:11 +0100

    Add status message when sorting for screen readers

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,6 @@
 /*
- *   This content is licensed according to the W3C Software License at
- *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
- *
- *   File:   sortable-table.js
+ *   This is a modified version of Sortable Table Example:
+ *   https://w3c.github.io/aria-practices/examples/table/sortable-table.html
  *
  *   Desc:   Adds sorting to a HTML data table that implements ARIA Authoring Practices
  */
@@ -10,51 +8,23 @@
 class SortableTable {
   constructor(tableNode) {
     this.tableNode = tableNode;
-
-    this.columnHeaders = tableNode.querySelectorAll('thead th.sortable-col');
-
+    this.columnHeaders = tableNode.querySelectorAll('thead th');
     this.sortColumns = [];
 
     for (let i = 0; i < this.columnHeaders.length; i += 1) {
       const ch = this.columnHeaders[i];
-      const headerText = ch.innerHTML;
-      const headerButton = `<button>${headerText}</button>`;
-      ch.innerHTML = headerButton;
 
-      const buttonNode = ch.querySelector('button');
-      if (buttonNode) {
+      if (ch.classList.contains('sortable-col')) {
+        const headerText = ch.innerHTML;
+        const headerButton = `<button data-column-index="${i}">${headerText}<span aria-hidden="true"></span></button>`;
+        ch.innerHTML = headerButton;
         this.sortColumns.push(i);
-        buttonNode.setAttribute('data-column-index', i);
-        buttonNode.addEventListener('click', this.handleClick.bind(this));
+        ch.querySelector('button').addEventListener('click', this.handleClick.bind(this));
       }
     }
 
-    this.divHtml = '<div aria-live="polite" role="status" aria-atomic="true" class="nhsuk-u-visually-hidden"></div>';
-    this.tableNode.insertAdjacentHTML('afterend', this.divHtml);
-
-    /*
-    this.optionCheckbox = document.querySelector(
-      'input[type="checkbox"][value="show-unsorted-icon"]'
-    );
-    */
-    /*
-    if (this.optionCheckbox) {
-      this.optionCheckbox.addEventListener(
-        'change',
-        this.handleOptionChange.bind(this)
-      );
-      if (this.optionCheckbox.checked) {
-        this.tableNode.classList.add('show-unsorted-icon');
-      }
-    }
-    */
-
-    // var tableObj = document.querySelector('table.sortable');
-    // eslint-disable-next-line max-len
-    // var ariaHtml = '<div id="message" aria-live="polite" role="status" aria-atomic="true" class="nhsuk-u-visually-hidden"></div>';
-    // tableObj.insertAdjacentHTML("afterend", ariaHtml);
-    // eslint-disable-next-line max-len
-    // this.tableNode.insertAdjacentHTML("afterend", '<div id="message" aria-live="polite" role="status" aria-atomic="true" class="nhsuk-u-visually-hidden"></div>');
+    this.divHtml = '<div id="message" aria-live="polite" role="status" aria-atomic="true" class="nhsuk-u-visually-hidden"></div>';
+    this.tableNode.insertAdjacentHTML('beforebegin', this.divHtml);
   }
 
   setColumnHeaderSort(columnIndex) {
@@ -76,7 +46,7 @@ class SortableTable {
             ch.classList.contains('num'),
           );
           this.updateStatus(
-            `Sort by ${buttonNode.innerText} (ascending)`,
+            `Sorted by ${buttonNode.innerText} (ascending)`,
           );
         } else {
           ch.setAttribute('aria-sort', 'descending');
@@ -86,7 +56,7 @@ class SortableTable {
             ch.classList.contains('num'),
           );
           this.updateStatus(
-            `Sort by ${buttonNode.innerText} (descending)`,
+            `Sorted by ${buttonNode.innerText} (descending)`,
           );
         }
       } else if (ch.hasAttribute('aria-sort') && buttonNode) {
@@ -95,10 +65,10 @@ class SortableTable {
     }
   }
 
+  // eslint-disable-next-line class-methods-use-this
   updateStatus(statusMessage) {
-    const messageDiv = this.tableNode.nextElementSibling;
-    const mesg = statusMessage;
-    messageDiv.innerHTML = mesg;
+    const messageDiv = document.querySelector("[role='status']");
+    messageDiv.textContent = statusMessage;
   }
 
   sortColumn(columnIndex, sortValue, isNumber) {
@@ -163,31 +133,15 @@ class SortableTable {
   }
 
   /* EVENT HANDLERS */
-
   handleClick(event) {
     const tgt = event.currentTarget;
     this.setColumnHeaderSort(tgt.getAttribute('data-column-index'));
-    // console.log('clicked');
-    // this.updateStatus();
   }
-
-  /*
-  handleOptionChange(event) {
-    var tgt = event.currentTarget;
-
-    if (tgt.checked) {
-      this.tableNode.classList.add('show-unsorted-icon');
-    } else {
-      this.tableNode.classList.remove('show-unsorted-icon');
-    }
-  }
-  */
 }
 
 // Initialize sortable table buttons
 window.addEventListener('load', () => {
   const sortableTables = document.querySelectorAll('table.sortable');
-
   for (let i = 0; i < sortableTables.length; i += 1) {
     // eslint-disable-next-line no-new
     new SortableTable(sortableTables[i]);
